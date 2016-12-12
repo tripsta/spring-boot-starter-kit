5584b42e7139b704f56afa6259b615bc9bfef280
George Yord <george.yord@gmail.com>
2016-12-12 18:39:18 +0200

    Introduce new ping healthcheck endpoint

---
+++ application/web/src/main/java/com/sbsk/web/configuration/ActuatorConfiguration.java
@@ -0,0 +1,35 @@
+package com.sbsk.web.configuration;
+
+import org.springframework.boot.actuate.endpoint.Endpoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.sbsk"})
+public class ActuatorConfiguration {
+    @Bean
+    public static Endpoint<String> pingEndpoint() {
+        return new Endpoint<String>() {
+            @Override
+            public String getId() {
+                return "ping";
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+
+            @Override
+            public boolean isSensitive() {
+                return false;
+            }
+
+            @Override
+            public String invoke() {
+                return "OK";
+            }
+        };
+    }
+}
