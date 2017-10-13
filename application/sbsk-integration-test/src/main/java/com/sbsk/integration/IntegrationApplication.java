650386fe91ef9341fb0776659ca70c7c8c6650e6
Yiannis Dimitriou <ydimitriou@tripsta.com>
2017-10-13 16:17:38 +0300

    add sbsk integration test sub module

---
+++ application/sbsk-integration-test/src/main/java/com/sbsk/integration/IntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.sbsk.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationApplication.class, args);
+	}
+}
