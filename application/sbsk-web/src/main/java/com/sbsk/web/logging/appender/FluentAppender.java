86bbcedbfd27823c4eb61499ff3ed1c95af13df7
Nikos Voulgaris <nivoul@gmail.com>
2018-03-12 10:03:17 +0200

    Enhance fluent appender with environment field and profile specific configuration

--- application/sbsk-web/src/main/java/com/sbsk/web/logging/appender/FluentAppender.java
+++ application/sbsk-web/src/main/java/com/sbsk/web/logging/appender/FluentAppender.java
@@ -15,16 +15,28 @@ import java.util.concurrent.Executors;
 
 public class FluentAppender extends AppenderBase<ILoggingEvent> {
 
+  private static final String HOST_KEY = "host";
+  private static final String APP_KEY = "app";
+  private static final String ENV_KEY = "environment";
+  private static final String MAIN_KEY = "main";
+  private static final String THREAD_KEY = "thread";
+  private static final String LEVEL_KEY = "level";
+  private static final String CLASS_KEY = "class";
+  private static final String TYPE_KEY = "type";
+  private static final String EXCEPTIONS_KEY = "exceptions";
+  private static final String MESSAGE_KEY = "message";
+  private static final String STACKTRACE_KEY = "stacktrace";
+  private static final String TYPE_EXCEPTION = "EXCEPTION";
+  private static final String TYPE_MESSAGE = "MESSAGE";
+
   private FluentLogger fluentLogger;
   private ExecutorService service;
   private String tag;
   private String label;
+  private String environment;
   private String remoteHost;
   private int port;
   private int maxThreads;
-
-  public FluentAppender() {
-  }
 
   public String getTag() {
     return tag;
@@ -40,6 +52,14 @@ public class FluentAppender extends AppenderBase<ILoggingEvent> {
 
   public void setLabel(String label) {
     this.label = label;
+  }
+
+  public String getEnvironment() {
+    return environment;
+  }
+
+  public void setEnvironment(String environment) {
+    this.environment = environment;
   }
 
   public String getRemoteHost() {
@@ -69,7 +89,6 @@ public class FluentAppender extends AppenderBase<ILoggingEvent> {
   @Override
   public void start() {
     try {
-      System.out.println("Host: " + remoteHost + ", Port: " + port);
       fluentLogger = FluentLogger.getLogger(tag, remoteHost, port);
       this.service = Executors.newFixedThreadPool(maxThreads);
     } catch (RuntimeException e) {
@@ -90,32 +109,57 @@ public class FluentAppender extends AppenderBase<ILoggingEvent> {
 
   @Override
   protected void append(ILoggingEvent event) {
-    Map<String, Object> messages = new HashMap<String, Object>();
+    Map<String, Object> messages = new HashMap<>();
 
-    messages.put("host", getHostname());
-    messages.put("app", label);
-    messages.put("main", System.getProperty("sun.java.command"));
-    messages.put("thread", event.getThreadName());
-    messages.put("level", event.getLevel().toString());
-    messages.put("class", event.getLoggerName());
-    if (event.getThrowableProxy() == null) { // Without exception
-      messages.put("exception", "--- This.log.is.not.an.exception ---");
-      Throwable tempThrowable = new Throwable();
-      tempThrowable.setStackTrace(event.getCallerData());
-      messages.put("message", event.getFormattedMessage().toString());
-      if (event.getCallerData() != null) {
-        messages.put("stacktrace", extractCallerData(tempThrowable));
-      }
-    } else { // With exception
-      ThrowableProxy throwableProxy = (ThrowableProxy) event.getThrowableProxy();
-      messages.put("exception", throwableProxy.getClassName());
-      messages.put("message", throwableProxy.getMessage());
-      messages.put("stacktrace", extractCallerData(throwableProxy.getThrowable()));
+    addBaseData(event, messages);
+    if (eventHasExceptions(event)) {
+      addExceptionData(event, messages);
+    } else {
+      addMessageData(event, messages);
     }
+
     Long timeStamp = event.getTimeStamp() / 1000;
-    service.submit(() -> {
-      fluentLogger.log(label, messages, timeStamp);
-    });
+    service.submit(() -> fluentLogger.log(label, messages, timeStamp));
+  }
+
+  private void addBaseData(ILoggingEvent event, Map<String, Object> messages) {
+    messages.put(HOST_KEY, getHostname());
+    messages.put(APP_KEY, label);
+    messages.put(ENV_KEY, environment);
+    messages.put(MAIN_KEY, System.getProperty("sun.java.command"));
+    messages.put(THREAD_KEY, event.getThreadName());
+    messages.put(LEVEL_KEY, event.getLevel().toString());
+    messages.put(CLASS_KEY, event.getLoggerName());
+  }
+
+  private boolean eventHasExceptions(ILoggingEvent event) {
+    return event.getThrowableProxy() != null;
+  }
+
+  private void addExceptionData(ILoggingEvent event, Map<String, Object> messages) {
+    ThrowableProxy throwableProxy = (ThrowableProxy) event.getThrowableProxy();
+    messages.put(EXCEPTIONS_KEY, throwableProxy.getClassName());
+    messages.put(TYPE_KEY, TYPE_EXCEPTION);
+    messages.put(MESSAGE_KEY, throwableProxy.getMessage());
+    messages.put(STACKTRACE_KEY, extractCallerData(throwableProxy.getThrowable()));
+  }
+
+  private void addMessageData(ILoggingEvent event, Map<String, Object> messages) {
+    messages.put(TYPE_KEY, TYPE_MESSAGE);
+    if (eventContainsDataInMap(event)) {
+      addMapData(event.getArgumentArray()[0], messages);
+    } else {
+      messages.put(MESSAGE_KEY, event.getMessage());
+    }
+  }
+
+  private boolean eventContainsDataInMap(ILoggingEvent event) {
+    return event.getArgumentArray() != null && event.getArgumentArray()[0] instanceof HashMap;
+  }
+
+  private void addMapData(Object obj, Map<String, Object> messages) {
+    HashMap<String, String> map = (HashMap<String, String>) obj;
+    map.forEach((key, value) -> messages.put(key, value));
   }
 
   private String getHostname() {
