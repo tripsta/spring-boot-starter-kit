a940de7188d753d3036ab4e351e21461e3eb2ac3
Giorgos Papaefthimiou <georgeyord@users.noreply.github.com>
2018-04-06 10:26:47 +0300

    Update rename_modules.sh

--- scripts/rename_modules.sh
+++ scripts/rename_modules.sh
@@ -3,16 +3,17 @@
 CURRENT_PATH=`pwd`
 
 if [[ "${FROM_MODULE}" == "" ]]; then
-  echo -e 'Usage:\nFROM_MODULE="sbsk" TO_MODULE="tripsta.talos" scripts/rename_modules.sh'
+  echo -e 'Usage:\nFROM_MODULE="sbsk" TO_MODULE="tripsta.talos" APPLICATION_NAME="talos" scripts/rename_modules.sh'
   exit 1
 fi
 
-if [[ "${FROM_MODULE}" == "" ]]; then
-  echo "FROM_MODULE env variable is needed"
-  exit 1
-fi
 if [[ "${TO_MODULE}" == "" ]]; then
   echo "TO_MODULE env variable is needed"
+  exit 1
+fi
+
+if [[ "${APPLICATION_NAME}" == "" ]]; then
+  echo "APPLICATION_NAME env variable is needed"
   exit 1
 fi
 
@@ -21,13 +22,26 @@ echo -e "### CURRENT_PATH: ${CURRENT_PATH}"
 echo -e "\n\n### Replacing content in files from '${FROM_MODULE}' to '${TO_MODULE}' in '${CURRENT_PATH}'"
 egrep -lir "(${FROM_MODULE})" ${CURRENT_PATH} |\
   grep '.java\|.xml' |\
+  grep -v ".git" |\
   grep -v '/build/' |\
   while read TARGET; do
-    echo "sed -i 's/${FROM_MODULE}/${TO_MODULE}/g' $TARGET"
+    echo "sed -i'.bak' -e 's/${FROM_MODULE}/${TO_MODULE}/g' $TARGET"
   done
 
+echo -e "\n\n### Replacing content in files from '${FROM_MODULE}' to '${TO_MODULE}' in '${CURRENT_PATH}'"
+egrep -lir "(${FROM_MODULE})" ${CURRENT_PATH} |\
+  grep '.gradle' |\
+  grep -v ".git" |\
+  grep -v '/build/' |\
+  while read TARGET; do
+    echo "sed -i'.bak' -e 's/${FROM_MODULE}/${APPLICATION_NAME}/g' $TARGET"
+  done
+
+echo -e "\n\n### Removing .bak filers generqted from sed"
+echo -e "find . -name '*.bak' -type f -delete"
+
 FROM_PATH="${FROM_MODULE/./\/}"
-TO_PATH="${TO_MODULE/./\/}"
+TO_PATH="${APPLICATION_NAME/./\/}"
 echo -e "\n\n### Renaming files from '${FROM_PATH}' to '${TO_PATH}' in '${CURRENT_PATH}'"
 find ${CURRENT_PATH} -type d -print |\
   grep "${FROM_PATH}$" |\
@@ -35,18 +49,18 @@ find ${CURRENT_PATH} -type d -print |\
   grep -v "build.gradle" |\
   while read FROM; do
     TO="${FROM/${FROM_PATH}/${TO_PATH}}"
-    echo -e "mkdir -p `dirname ${TO}`"
-    echo -e "cp -RT ${FROM} ${TO}"
+    echo -e "mkdir -p `dirname ${TO}` && \ "
+    echo -e "cp -R ${FROM} ${TO} && \ "
     echo -e "rm -rf ${FROM}\n"
   done
 
+echo -e "\n\n### Renaming root folder to  ${APPLICATION_NAME}"
+echo -e "mv ${CURRENT_PATH}/application ${CURRENT_PATH}/${APPLICATION_NAME}\n"
+
 echo -e "\n\n *** CHECK AND RUN THE COMMANDS ABOVE AT YOUR OWN RISK"
 
-echo -e "\n\n ### Remember to change manually the basename in 'build.gradle', if needed.\nPath: ${CURRENT_PATH}/application/build.gradle\nCurrent value:"
-cat "${CURRENT_PATH}/application/build.gradle" | grep "baseName"
+echo -e "\n\n ### Remember to change manually the basename in 'build.gradle', if needed.\nPath: ${CURRENT_PATH}/${APPLICATION_NAME}/build.gradle\nCurrent value:"
+cat "${CURRENT_PATH}/${APPLICATION_NAME}/build.gradle" | grep "baseName"
 
-echo -e "\n\n ### Also, remember to change manually the display-name in 'application.yml', if needed.\nPath: ${CURRENT_PATH}/application/web/src/main/resources/config/application.yml\nCurrent value:"
-cat "${CURRENT_PATH}/application/web/src/main/resources/config/application.yml" | grep "display-name"
-echo \
-464 \
-76576
+echo -e "\n\n ### Also, remember to change manually the display-name in 'application.yml', if needed.\nPath: ${CURRENT_PATH}/${APPLICATION_NAME}/web/src/main/resources/config/application.yml\nCurrent value:"
+cat "${CURRENT_PATH}/${APPLICATION_NAME}/web/src/main/resources/config/application.yml" | grep "display-name"
